c204f2475a6b8bb8a32a7617580d7c52858b2cc0
lRaika <arnurelnur@gmail.com>
2017-10-10 17:29:58 +0600

    HomeWork
    HomeWorkDone

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonInYear = new System.Windows.Forms.RadioButton();
+            this.radioButtonInMonth = new System.Windows.Forms.RadioButton();
+            this.radioButtonInDay = new System.Windows.Forms.RadioButton();
+            this.radioButtonInMinutes = new System.Windows.Forms.RadioButton();
+            this.radioButtonInSeconds = new System.Windows.Forms.RadioButton();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // radioButtonInYear
+            // 
+            this.radioButtonInYear.AutoSize = true;
+            this.radioButtonInYear.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonInYear.Location = new System.Drawing.Point(219, 18);
+            this.radioButtonInYear.Name = "radioButtonInYear";
+            this.radioButtonInYear.Size = new System.Drawing.Size(79, 23);
+            this.radioButtonInYear.TabIndex = 1;
+            this.radioButtonInYear.TabStop = true;
+            this.radioButtonInYear.Text = "В годах";
+            this.radioButtonInYear.UseVisualStyleBackColor = true;
+            this.radioButtonInYear.CheckedChanged += new System.EventHandler(this.RadioButtonInYear_CheckedChanged);
+            // 
+            // radioButtonInMonth
+            // 
+            this.radioButtonInMonth.AutoSize = true;
+            this.radioButtonInMonth.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonInMonth.Location = new System.Drawing.Point(219, 47);
+            this.radioButtonInMonth.Name = "radioButtonInMonth";
+            this.radioButtonInMonth.Size = new System.Drawing.Size(98, 23);
+            this.radioButtonInMonth.TabIndex = 2;
+            this.radioButtonInMonth.TabStop = true;
+            this.radioButtonInMonth.Text = "В месяцах";
+            this.radioButtonInMonth.UseVisualStyleBackColor = true;
+            this.radioButtonInMonth.CheckedChanged += new System.EventHandler(this.RadioButtonInMonth_CheckedChanged);
+            // 
+            // radioButtonInDay
+            // 
+            this.radioButtonInDay.AutoSize = true;
+            this.radioButtonInDay.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonInDay.Location = new System.Drawing.Point(219, 76);
+            this.radioButtonInDay.Name = "radioButtonInDay";
+            this.radioButtonInDay.Size = new System.Drawing.Size(73, 23);
+            this.radioButtonInDay.TabIndex = 3;
+            this.radioButtonInDay.TabStop = true;
+            this.radioButtonInDay.Text = "В днях";
+            this.radioButtonInDay.UseVisualStyleBackColor = true;
+            this.radioButtonInDay.CheckedChanged += new System.EventHandler(this.RadioButtonInDay_CheckedChanged);
+            // 
+            // radioButtonInMinutes
+            // 
+            this.radioButtonInMinutes.AutoSize = true;
+            this.radioButtonInMinutes.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonInMinutes.Location = new System.Drawing.Point(219, 105);
+            this.radioButtonInMinutes.Name = "radioButtonInMinutes";
+            this.radioButtonInMinutes.Size = new System.Drawing.Size(98, 23);
+            this.radioButtonInMinutes.TabIndex = 4;
+            this.radioButtonInMinutes.TabStop = true;
+            this.radioButtonInMinutes.Text = "В минутах";
+            this.radioButtonInMinutes.UseVisualStyleBackColor = true;
+            this.radioButtonInMinutes.CheckedChanged += new System.EventHandler(this.RadioButtonInMinutes_CheckedChanged);
+            // 
+            // radioButtonInSeconds
+            // 
+            this.radioButtonInSeconds.AutoSize = true;
+            this.radioButtonInSeconds.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonInSeconds.Location = new System.Drawing.Point(219, 134);
+            this.radioButtonInSeconds.Name = "radioButtonInSeconds";
+            this.radioButtonInSeconds.Size = new System.Drawing.Size(104, 23);
+            this.radioButtonInSeconds.TabIndex = 5;
+            this.radioButtonInSeconds.TabStop = true;
+            this.radioButtonInSeconds.Text = "В секундах";
+            this.radioButtonInSeconds.UseVisualStyleBackColor = true;
+            this.radioButtonInSeconds.CheckedChanged += new System.EventHandler(this.RadioButtonInSeconds_CheckedChanged);
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(355, 18);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.ReadOnly = true;
+            this.textBoxInfo.Size = new System.Drawing.Size(337, 139);
+            this.textBoxInfo.TabIndex = 6;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(13, 18);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker.TabIndex = 7;
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.DateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.radioButtonInSeconds);
+            this.Controls.Add(this.radioButtonInMinutes);
+            this.Controls.Add(this.radioButtonInDay);
+            this.Controls.Add(this.radioButtonInMonth);
+            this.Controls.Add(this.radioButtonInYear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButtonInYear;
+        private System.Windows.Forms.RadioButton radioButtonInMonth;
+        private System.Windows.Forms.RadioButton radioButtonInDay;
+        private System.Windows.Forms.RadioButton radioButtonInMinutes;
+        private System.Windows.Forms.RadioButton radioButtonInSeconds;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+    }
+}
+
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        DateTimePicker dateTime = new DateTimePicker();
+        bool lessDate = false;
+        int index = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            dateTime.Value = dateTimePicker.Value;
+        }
+
+        private void GetResults()
+        {
+            if (lessDate)
+            {
+                textBoxInfo.Text= "Выбранная дата уже прошла!";
+            }
+            else
+            {
+                switch (index)
+                {
+                    case 0:
+                        textBoxInfo.Text = "";
+                        break;
+                    case 1:
+                        textBoxInfo.Text = "Осталось  " +GetData(index) +"  года";
+                        break;
+                    case 2:
+                        textBoxInfo.Text = "Осталось  " + GetData(index) + "  месяцов";
+                        break;
+                    case 3:
+                        textBoxInfo.Text = "Осталось  " + GetData(index) + "  дней";
+                        break;
+                    case 4:
+                        textBoxInfo.Text = "Осталось  " + GetData(index) + "  минут";
+                        break;
+                    case 5:
+                        textBoxInfo.Text = "Осталось  " + GetData(index) + "  секунд";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+        private string GetData(int index)
+        {
+            int intData;
+            double doubleData;
+            int dayInYear = 365;
+            int avarageDayInMonth = 30;
+            int monthInYear = 12;
+            int minutesInDay = 1440;
+            int secondsInDay = 86400;
+            double dateInMonthDateTimePicker = dateTimePicker.Value.Year * monthInYear + dateTimePicker.Value.Month;
+            double dateInDayDateTimePicker = dateTimePicker.Value.Year * dayInYear + dateTimePicker.Value.Month*avarageDayInMonth+dateTimePicker.Value.Day;
+            double dateInMonthDateTime = dateTime.Value.Year * monthInYear + dateTime.Value.Month;
+            double dateInDayDateTime = dateTime.Value.Year * dayInYear + dateTime.Value.Month * avarageDayInMonth + dateTime.Value.Day;
+            int DateInDayTimePickerInt = dateTimePicker.Value.Year * monthInYear + dateTimePicker.Value.Month;
+            int dateInDayDateTimePickerInt = dateTimePicker.Value.Year * dayInYear + dateTimePicker.Value.Month * avarageDayInMonth + dateTimePicker.Value.Day;
+            int dateInMonthDateTimeInt = dateTime.Value.Year * monthInYear + dateTime.Value.Month;
+            int dateInDayDateTimeInt = dateTime.Value.Year * dayInYear + dateTime.Value.Month * avarageDayInMonth + dateTime.Value.Day;
+            switch (index)
+            {
+
+                case 0:
+                    return "";
+                case 1:
+                    doubleData = (dateInMonthDateTimePicker - dateInMonthDateTime) / monthInYear;
+                    return doubleData.ToString();
+                case 2:
+                    doubleData = (dateInDayDateTimePicker - dateInDayDateTime) / dayInYear;
+                    return doubleData.ToString();
+                case 3:
+                    intData = dateInDayDateTimePickerInt - dateInDayDateTimeInt;
+                    return intData.ToString();
+                case 4:
+                    intData = (dateInDayDateTimePickerInt - dateInDayDateTimeInt) * minutesInDay;
+                    return intData.ToString();
+                case 5:
+                    intData = (dateInDayDateTimePickerInt - dateInDayDateTimeInt) * secondsInDay;
+                    return intData.ToString();
+                default:
+                    return"";
+            }
+        }
+        private void RadioButtonInYear_CheckedChanged(object sender, EventArgs e)
+        {
+            index = 1;
+            GetResults();
+        }
+
+        private void RadioButtonInMonth_CheckedChanged(object sender, EventArgs e)
+        {
+            index = 2;
+            GetResults();
+        }
+
+        private void RadioButtonInDay_CheckedChanged(object sender, EventArgs e)
+        {
+            index = 3;
+            GetResults();
+        }
+
+        private void RadioButtonInMinutes_CheckedChanged(object sender, EventArgs e)
+        {
+            index = 4;
+            GetResults();
+        }
+
+        private void RadioButtonInSeconds_CheckedChanged(object sender, EventArgs e)
+        {
+            index = 5;
+            GetResults();
+        }
+
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            if (dateTimePicker.Value <= dateTime.Value)
+                lessDate = true;
+            else
+                lessDate = false;
+            GetResults();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
